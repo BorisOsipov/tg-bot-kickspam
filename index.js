c9cb6a83c3f25140ea19d3fc0d049a3dc15c03ec
Boris Osipov <osipov.boris@gmail.com>
2018-09-19 22:33:51 +0300

    Ban forward messages.

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 
-const token = process.env.TG_TOKEN;
+const token = process.env.TG_TOKEN || '698999729:AAFQr-NAYU5rVpKVktCp-foA3FR4WM6VSCg';
 const bot = new TelegramBot(token, { polling: true });
-const banPhrases = config.banPhrases.map(el => el.toLowerCase());
 const { watchGroupNames } = config;
 
-const clearMessage = async (tgMessage) => {
-  const isForbiddenMessage = banPhrases.find((phrase) => {
-    if (!tgMessage.text) {
-      return false;
+const TELEGRAM_URL_REGEX = /(https?:\/\/)?(www[.])?(telegram|t)\.me\/joinchat\/([a-zA-Z0-9_-]*)\/?/ig;
+
+const isMessageContainsLink = (message) => {
+  let isLink = false;
+
+  if (message.forward_from_message_id) {
+    if (message.caption) {
+      isLink = isLink || TELEGRAM_URL_REGEX.test(message.caption);
     }
-    const text = tgMessage.text.toLowerCase();
-    return text.includes(phrase);
-  });
+  }
+  return isLink;
+};
+
+const performActionsToSender = async (tgMessage) => {
+  await bot.deleteMessage(tgMessage.chat.id, tgMessage.message_id);
+  await bot.kickChatMember(tgMessage.chat.id, tgMessage.from.id);
+};
+
+const clearMessage = async (tgMessage) => {
+  const isLink = isMessageContainsLink(tgMessage);
 
   const isWatchedChannel = watchGroupNames.find(name =>
     (tgMessage.chat.username ? name === tgMessage.chat.username : false));
 
-  if (isWatchedChannel && isForbiddenMessage) {
+  if (isWatchedChannel && isLink) {
     try {
-      await bot.kickChatMember(tgMessage.chat.id, tgMessage.from.id);
-      await bot.deleteMessage(tgMessage.chat.id, tgMessage.message_id);
+      await performActionsToSender(tgMessage);
+      const message = `
+Remove and ban message
+From: ${tgMessage.from.first_name} ${tgMessage.from.last_name} @${tgMessage.from.username}
+Text: ${tgMessage.text}
+Link: https://t.me/${tgMessage.chat.username}/${tgMessage.message_id}
+`;
+      await bot.sendMessage(config.forwardToChanelId, message);
     } catch (e) {
       console.log(`Error: ${e}`);
     }
