e2dd993a6244cac6be6fdd4d00634e04f6a1bd8c
Boris Osipov <osipov.boris@gmail.com>
2018-10-07 18:08:00 +0300

    Set log rotate options.

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,26 @@
 import * as path from "path";
 import * as winston from "winston";
 
+const maxsize = 2000 * 1000 * 10;
+const level = "debug";
+
 export const banLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({level: "debug", filename: path.join(__dirname, "/logs/bans.log")}),
+    new winston.transports.File({maxsize, level, filename: path.join(__dirname, "/logs/bans.log")}),
   ],
 });
 
 export const messagesLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({level: "debug", filename: path.join(__dirname, "/logs/messages.log")}),
+    new winston.transports.File({maxsize, level, filename: path.join(__dirname, "/logs/messages.log")}),
   ],
 });
 
 export const errorLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({level: "debug", filename: path.join(__dirname, "/logs/error.log")}),
+    new winston.transports.File({maxsize, level, filename: path.join(__dirname, "/logs/error.log")}),
   ],
 });
