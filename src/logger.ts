0e90705e254f28bf6836d63ab0da7611976934fd
Boris Osipov <osipov.boris@gmail.com>
2018-10-07 18:00:03 +0300

    Fix log folder

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,23 @@
+import * as path from "path";
 import * as winston from "winston";
 
 export const banLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({level: "debug", filename: "./bans.log"}),
+    new winston.transports.File({level: "debug", filename: path.join(__dirname, "/logs/bans.log")}),
   ],
 });
 
 export const messagesLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({level: "debug", filename: "./messages.log"}),
+    new winston.transports.File({level: "debug", filename: path.join(__dirname, "/logs/messages.log")}),
   ],
 });
 
 export const errorLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({level: "debug", filename: "./error.log"}),
+    new winston.transports.File({level: "debug", filename: path.join(__dirname, "/logs/error.log")}),
   ],
 });
